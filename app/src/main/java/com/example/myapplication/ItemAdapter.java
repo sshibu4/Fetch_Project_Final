297ca11e8345fe7fdc94d2115e200e3e1cf07a68
Simon Shibu <simonshibu2001@gmail.com>
2024-02-20 18:48:20 -0600

    Done with Fetch Project

--- app/src/main/java/com/example/myapplication/ItemAdapter.java
+++ app/src/main/java/com/example/myapplication/ItemAdapter.java
@@ -8,14 +8,17 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
+// Adapter for RecyclerView that binds Item data to views that are displayed within a RecyclerView
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
 
+    // List to hold Item objects, which is the data source for the adapter.
     private List<Item> itemList; // The data source of the adapter
 
     public ItemAdapter(List<Item> itemList) {
         this.itemList = itemList;
     }
 
+    // Method to create new ViewHolder objects (invoked by the layout manager).
     @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -24,6 +27,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
         return new ItemViewHolder(itemView);
     }
 
+    // Method to bind data to the ViewHolder (invoked by the layout manager).
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
         Item item = itemList.get(position);
@@ -32,14 +36,17 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
         holder.textViewName.setText(String.format("Name: %s", item.getName()));
     }
 
+    // Method to get the size of the data source (invoked by the layout manager).
     @Override
     public int getItemCount() {
         return itemList.size();
     }
 
+    // ViewHolder class that holds references to all views within each item row.
     public static class ItemViewHolder extends RecyclerView.ViewHolder {
         TextView textViewId, textViewListId, textViewName;
 
+        // Constructor for the ViewHolder, with itemView as the inflated layout.
         public ItemViewHolder(View itemView) {
             super(itemView);
             textViewId = itemView.findViewById(R.id.textViewId);
--- app/src/main/java/com/example/myapplication/ItemRepository.java
+++ app/src/main/java/com/example/myapplication/ItemRepository.java
@@ -11,8 +11,11 @@ public class ItemRepository {
 
     private ApiService apiService;
 
+    // The ItemRepository class is responsible for setting up the Retrofit instance
+    // and providing a method to fetch items from the remote data source.
     public ItemRepository(){
-
+        // Building the Retrofit instance using its Builder class
+        // This instance will be used to send out network requests to the specified BASE URL
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://fetch-hiring.s3.amazonaws.com/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -21,6 +24,8 @@ public class ItemRepository {
         apiService = retrofit.create(ApiService.class);
     }
 
+    // Public method to initiate the fetching of items.
+    // The method takes a Callback<List<Item>> to handle the results asynchronously.
     public void getItems(Callback<List<Item>> callback) {
         Call<List<Item>> call = apiService.getItems();
         call.enqueue(callback);
